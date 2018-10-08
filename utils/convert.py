b35075193ebad09b848a0837e12f2bbc5d202725
Alan Huang <Alan.Huang@utdallas.edu>
2018-10-08 11:01:04 -0500

    add bespoke setting information

--- utils/convert.py
+++ utils/convert.py
@@ -7,17 +7,5 @@ with open('sunless.dat', 'w') as f:
                 for line in data:
                     entry = {'key': f'{name}:{line["Id"]}', 'value': line}
                     f.write(f"{json.dumps(entry)}\n")
-    with open('Tiles.txt') as g:
-        tiles = json.loads(g.read())
-        ports = {}
-        for tile in tiles:
-            for subtile in tile['Tiles']:
-                for port in subtile['PortData']:
-                    id = port['Setting']['Id']
-                    try:
-                        ports[id].append(port['Name'])
-                    except KeyError:
-                        ports[id] = [port['Name']]
-        for id in ports:
-            entry = {'key': f'settings:{id}', 'value': {'Id': id, 'Name': ports[id][0] if len(ports[id]) == 1 else ports[id]}}
-            f.write(f'{json.dumps(entry)}\n')
+    with open('settings.json') as g:
+        f.write(g.read())
